b3ba51c305abdd84f31b8d40a33402b6b6358f26
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-07-04 15:15:11 +0530

    Connected to firebase

---
+++ app/src/main/java/com/pranavprksn/medt/ui/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.pranavprksn.medt.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pranavprksn.medt.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
